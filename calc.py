735dab8f9ccac3a10c14f9655f701870f016e54d
aidakaleb <48600056+aidakaleb@users.noreply.github.com>
2019-03-16 12:08:01 -0400

    finish day 0

---
+++ calc.py
@@ -0,0 +1,37 @@
+# print(64+39+47)
+# print("hello")
+# # print(2*100)
+# # print("hello")
+# print('Hey Jude', "Don't make it bad")
+# print('1+2+3=',1+2+3)
+# name = input()
+# print('hello,',name)
+# n=100
+# print(n)
+# # print(first_name*20)
+# first_name = 'john'
+# last_name = 'smith'
+# # name = first_name+""+last_name
+# # print(name)
+# template = 'hello,{}! You are {} years old'
+# # name1 = 'grace'
+# # age1 = 20
+# # name2 = 'Aida'
+# # age2 = 18
+# print(template.format(name1,age1))
+# print(template.format(name2,age2))
+# print('pi equals {:.2f}'.format(3.141592))
+# pi='pi equals {:.2f}'
+# print(pi.format(3.141592))
+# import math
+# print(math.pow(2,5))
+# print(math.pi)
+# NUMBER_OF_SECONDS_IN_ONE_MINUTE = 60
+# seconds= 42* NUMBER_OF_SECONDS_IN_ONE_MINUTE+ 42
+# print("There are {} seconds...".format(seconds))
+#exersise 2.2
+NUMBER_OF_KILOMETERS_IN_A_MILE = 1.61
+miles= 10/NUMBER_OF_KILOMETERS_IN_A_MILE
+print("There are {:.2f} kilometers in 10 miles".format(miles))
+
+
